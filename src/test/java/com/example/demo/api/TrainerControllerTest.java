e1986797b1da8d9398942b41f951921997ea13ed
RiverAndBoat <842714673@qq.com>
2020-09-24 21:55:30 +0800

    [Jiang Yuzhou] feat: 周末补测试！！！

---
+++ src/test/java/com/example/demo/api/TrainerControllerTest.java
@@ -0,0 +1,10 @@
+package com.example.demo.api;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(TrainerController.class)
+class TrainerControllerTest {
+
+}
---
+++ src/test/java/com/example/demo/api/aggregation/GroupingControllerTest.java
@@ -0,0 +1,10 @@
+package com.example.demo.api.aggregation;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(GroupingController.class)
+class GroupingControllerTest {
+
+}
